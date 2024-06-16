b6ae107f3f174775ff3c1c4ad598a48dd8e46804
zebbaukhagen <69488628+zebbaukhagen@users.noreply.github.com>
2024-06-16 12:48:15 -0400

    Add tutorial text, modify test scene

--- v_00_ProjectOpera/Assets/Scripts/TutorialText.cs
+++ v_00_ProjectOpera/Assets/Scripts/TutorialText.cs
@@ -7,7 +7,13 @@ public class TutorialText
         "Would you like a refresher on how to run the ship?",
         "Okay! Let's talk nuts and bolts.",
         "Here at T&T, we do the right thing. So your ship is equipped with a lossless recycling system.",
-        "This is simply to test the system.",
+        "That means that we don't waste anything!.",
+        "When you get up, you'll see the trade interface at the front of your ship.",
+        "Don't sell everything right away! You're going to need it.",
+        "Around your ship you will see machine slots. You can use them to build machines.",
+        "Machines take input, and produce a product. Use the machines to multiply your resources over time!",
+        "Then, when you have a surplus of something, you can sell it at the terminal for credits.",
+        "You can use credits to buy more machines, or pay off your debt to the corporation."
         "Okay, let's get started then!"
     };
 
