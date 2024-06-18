6a798f6fc07e2a7cc7993c6f9038b6bada5d6450
Rachel Huggins <rhuggins7441@gmail.com>
2024-06-18 12:50:27 -0400

    Icon Color Change

---
+++ v_00_ProjectOpera/Assets/Scripts/UI/IconTint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconTint : MonoBehaviour
+{
+    [SerializeField] private Image[] sprites;
+    [SerializeField] private Color tint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < sprites.Length; i++)
+        {
+            sprites[i].color = tint;
+        }
+    }
+}
