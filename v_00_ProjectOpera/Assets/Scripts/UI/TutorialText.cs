6dd57400646a4ae9aa9e734ee3388009061ccc6f
zebbaukhagen <69488628+zebbaukhagen@users.noreply.github.com>
2024-07-08 23:15:05 -0400

    Fix tutorialText

--- v_00_ProjectOpera/Assets/Scripts/UI/TutorialText.cs
+++ v_00_ProjectOpera/Assets/Scripts/UI/TutorialText.cs
@@ -3,14 +3,12 @@ public class TutorialText
     public string[] tutorialSteps = new string[] {
         "Welcome, to Trade 'n Transit!",
         "Would you like a refresher on how to run the ship?",
-        "Okay! your ship is equipped with a lossless recycling system.",
-        "Here at T&T, we do the right thing. So your ship is equipped with a lossless recycling system.",
+        "Okay! Your ship is equipped with a lossless recycling system.",
         "That means that we don't waste anything!",
-        "Don't sell everything right away! You're going to need it.",
         "Around your ship you will see machine slots. You can use them to build machines.",
         "Machines take input, and produce a product. Use the machines to multiply your resources over time!",
         "Then, when you have a surplus of something, you can sell it at the terminal for credits.",
-        "You can use credits to buy more machines, or pay off your debt to the corporation.",
+        "You can use credits to buy more machines, and pay off your debt to the corporation.",
         "Let's get started then!"
     };
 }
